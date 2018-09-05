86789491b0cd3ad721fcd20bd92f385a8ee34f83
Victor Tsang <vtsang@netbase.com>
2018-09-04 22:22:04 -0700

    hellow world

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello World!</h1>
+    <p>My Name is Victor Tsang.</p>
+    <p>Welcome to my first gatsby site!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
